b0c327e37147ce9b5a9c6c1ef521ad2e9d02ab47
Vianney MERIGNARGUES (윤지후) <89965810+Vianney89@users.noreply.github.com>
2022-04-19 11:48:35 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
-let button = document.querrySelector("button");
+let button = document.querySelector("button");
 let div = document.querySelector("div");
+const classiferModule = document.querySelector("#classifier-Module");
 
 button.addEventListener("click",()=>{
+  const div = document.createElement('div');
+  div.classList = "classifier";
+  div.innerHTML = `
+  <header class="section header"></header>
+  <h2>Class</h2>
+  <span id="edit-icon-container"></span>
+  <hr class="line-style2">
+  <p>Add Image Samples:</p>`;
+
+  classiferModule.appendChild(div);
     
 })
 function showBlock(){
