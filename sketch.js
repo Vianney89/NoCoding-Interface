315e735d7f8188fa6ce09dfedf0cd24f4e389c40
Vianney MERIGNARGUES (윤지후) <89965810+Vianney89@users.noreply.github.com>
2022-04-28 14:44:12 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,45 @@
 let button = document.querySelector("#add-class");
 let div = document.querySelector("div");
 const classiferModule = document.querySelector("#classifier-Module");
+// console.log(classiferModule.hasChildNodes);
+let count =2;
+
 
 button.addEventListener("click",()=>{
   const div = document.createElement('div');
   div.classList = "classifier";
+  count++;
+
   div.innerHTML = `
   <header class="section header"></header>
-  <h2>Class</h2> 
+  <h2>Class ${count}</h2> 
   <button class="close-button">x</span>
   </button>
   <span id="edit-icon-container"></span>
   <hr class="line-style2">
   <p>Add Image Samples:</p>`;
 
-  // div.innerHTML =`
-  // <header class="section header"></header>
-  // <h2>Class<button id="close-button">X</button></h2>
-  // <span id="edit-icon-container"></span>
-  // <hr class="line-style">
-  // <p>Add Image Samples:</p>`;
-
-
+  var closeButton = div.getElementsByClassName("close-button");
+  var i;
+  
+  for (i = 0; i < closeButton.length; i++) {
+  closeButton[i].addEventListener("click", function () {
+    this.parentNode.style.display = "none";
+    count--;
+          });
+      };
+  
   classiferModule.appendChild(div);
-    
-})
-function showBlock(){
-  document.getElementsByClassName("add-class").style.display="";
-}
+});
 
-// close button 
-// window.onload = function(){
-//   document.getElementsByClassName("add-class").onclick = function(){
-//     this.parentNode.remove()
-//     return false;
-//   };
+// function showBlock(){
+//   document.getElementsByClassName("add-class").style.display="";
 // };
+
+
+// add a number for each div
+
+// button.addEventListener("click",()=>{
+//   div.innerText=count;
+//   count++;
+// })
